c09c8bed16e354a0a49e416a100a9af5a842224f
21Scoob <vgaleriu56@gmail.com>
2025-01-23 21:26:25 +0200

    setup front-end2

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="bg-gray-950 pb-24 pt-12 w-full px-36 text-gray-50 gap-24 min-h-screen">
+    <div className=" pb-24 pt-12 w-full px-36 text-gray-500 gap-24 min-h-screen">
       Hasulyt
     </div>
   );
