8657835eb2c6d4920e7b08269075c7e8c215633e
21Scoob <vgaleriu56@gmail.com>
2025-01-23 21:21:21 +0200

    setup front-end

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gray-950 pb-24 pt-12 w-full px-36 text-gray-50 gap-24 min-h-screen">
+      Hasulyt
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="bg-gray-950 pb-24 pt-12 w-full px-36 text-gray-50 gap-24 min-h-screen">
+      Hasulyt
+    </div>
+  );
+}
+
+export default App;
